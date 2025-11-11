b1751cb88f42a74743b0334c105727274a85c2d9
stefin <stefinracho@gmail.com>
2025-11-11 09:56:02 -0800

    more init. last commit of the day

--- allocator.py
+++ allocator.py
@@ -2,7 +2,7 @@ import random
 from optparse import OptionParser
 
 class Allocator:
-    def __init__(self, size, start, headerSize, policy):
+    def __init__(self, size, start, headerSize, policy, order, coalesce, align):
         # size of space
         self.size = size
 
@@ -18,4 +18,24 @@ class Allocator:
         
         #policy
         self.policy = policy
+            # assert checks if statement is true. if false halts program
+            #   here it has a list of strings, and compares self.policy to
+            #       the strings in the list
         assert(self.policy in ['FIRST', 'BEST', 'WORST'])
+
+        # list ordering
+        self.returnPolicy = order
+        assert(self.returnPolicy in ['ADDSORT', 'SIZESORT+', 'SIZESORT-', 'INSERT-FRONT', 'INSERT-BACK'])
+
+        # full list coalesce
+        self.coalese = coalesce
+
+        # alignment (will be -1 if no alignment)
+        self.align = align
+        # we do not want 0 or any negative other than -1
+        assert(self.align == -1 or self.align > 0)
+
+        def addToMap(self, addr, size):
+            assert(addr not in self.sizemap)
+            self.sizemap[addr] = size
+            #prints "adding", addr, "to map of size", size
